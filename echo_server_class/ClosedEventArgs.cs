d9ae1e3d0797ded7b9a4305ea64a3e31d259574e
geun25 <sas0898@naver.com>
2021-08-31 17:25:21 +0900

    Tcp echo 통신

---
+++ echo_server_class/ClosedEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace echo_server_class
+{
+    public delegate void ClosedEventHandler(object sender, ClosedEventArgs e);
+    public class ClosedEventArgs : EventArgs
+    {
+        public IPEndPoint RemoteEP
+        {
+            get;
+            private set;
+        }
+
+        public ClosedEventArgs(IPEndPoint remote_ep)
+        {
+            RemoteEP = remote_ep;
+        }
+
+        public string IPStr
+        {
+            get
+            {
+                return RemoteEP.Address.ToString();
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return RemoteEP.Port;
+            }
+        }
+    }
+}
