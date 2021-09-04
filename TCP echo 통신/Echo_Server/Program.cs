8a62594915e68469a8b42c500e888294a899dacb
geun25 <sas0898@naver.com>
2021-09-04 14:30:18 +0900

    TCP echo 통신

--- TCP echo 통신/Echo_Server/Program.cs
+++ TCP echo 통신/Echo_Server/Program.cs
@@ -66,7 +66,7 @@ namespace echo_server
                     string msg = br.ReadString();
                     br.Close();
                     ms.Close();
-                    
+
                     Console.WriteLine($"{iep.Address}:{iep.Port} -> {msg}");
                     if (msg == "exit")
                         break;
--- TCP echo 통신/Echo_Server/Properties/AssemblyInfo.cs
+++ TCP echo 통신/Echo_Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Network")]
+[assembly: AssemblyTitle("Echo_Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Network")]
+[assembly: AssemblyProduct("Echo_Server")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("a4b1aa33-d54f-4b4e-92b7-d8a55d3a0f46")]
+[assembly: Guid("350c6abf-2a3d-4b9c-ae56-30e1ae027e26")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
