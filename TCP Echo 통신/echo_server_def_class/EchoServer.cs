b5d5b23552c82f0632ab50a780152bbc328491ad
geun25 <78133537+geun25@users.noreply.github.com>
2021-09-04 15:13:20 +0900

    TCP echo 통신

---
+++ TCP Echo 통신/echo_server_def_class/EchoServer.cs
@@ -0,0 +1,128 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace echo_server_class
+{
+    public class EchoServer
+    {
+        public event AcceptedEventHandler AcceptedEventHandler = null;
+        public event ClosedEventHandler ClosedEventHandler = null;
+        public event RecvedMsgEventHandler RecvedMsgEventHandler = null;
+        public string IPStr
+        {
+            get;
+            private set;
+        }
+        public int Port
+        {
+            get;
+            private set;
+        }
+
+        public EchoServer(string ipstr, int port)
+        {
+            IPStr = ipstr;
+            Port = port;
+        }
+
+        Socket sock = null;
+        public bool Start()
+        {          
+            try
+            {
+                // 소켓 생성
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // 인터페이스와 결합
+                IPAddress addr = IPAddress.Parse(IPStr);
+                IPEndPoint iep = new IPEndPoint(addr, Port);
+                sock.Bind(iep);
+
+                // 백로그 큐 크기 설정
+                sock.Listen(5);                             
+                AcceptLoopAsync();
+                return true;
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+            if(sock != null)
+            {
+                try
+                {
+                    sock.Close();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        delegate void AcceptDele();
+        private void AcceptLoopAsync()
+        {
+            AcceptDele dele = AcceptLoop;
+            dele.BeginInvoke(null, null); // 비동기 실행
+        }
+
+        private void AcceptLoop()
+        {
+            Socket dosock = null;
+            while(true)
+            {
+                dosock = sock.Accept();
+                DoItAsync(dosock); // 비동기 실행
+            }
+        }
+
+        delegate void DoItDele(Socket dosock);
+        private void DoItAsync(Socket dosock)
+        {
+            DoItDele dele = DoIt;
+            dele.BeginInvoke(dosock, null, null);
+        }
+
+        private void DoIt(Socket dosock)
+        {
+            IPEndPoint remote_ep = dosock.RemoteEndPoint as IPEndPoint;
+            if(AcceptedEventHandler != null)            
+                AcceptedEventHandler(this, new AcceptedEventArgs(remote_ep));
+
+            try
+            {
+                byte[] packet = new byte[1024];                
+                while (true)
+                {
+                    dosock.Receive(packet);
+                    MemoryStream ms = new MemoryStream(packet);
+                    BinaryReader br = new BinaryReader(ms);
+                    string msg = br.ReadString();
+                    br.Close();
+                    ms.Close();
+
+                    if (RecvedMsgEventHandler != null)
+                        RecvedMsgEventHandler(this, new RecvedMsgEventArgs(remote_ep, msg));
+                                      
+                    dosock.Send(packet);
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                dosock.Close();
+                if (ClosedEventHandler != null)
+                    ClosedEventHandler(this, new ClosedEventArgs(remote_ep));
+            }
+        }
+    }
+}
---
+++ TCP Echo 통신/echo_server_def_class/RecvedMsgEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+
+namespace echo_server_class
+{
+    public delegate void RecvedMsgEventHandler(object sender, RecvedMsgEventArgs e);
+    public class RecvedMsgEventArgs : EventArgs
+    {
+        public IPEndPoint RemoteEP
+        {
+            get;
+            private set;
+        }
+
+        public string Msg
+        {
+            get;
+            private set;
+        }
+
+        public RecvedMsgEventArgs(IPEndPoint remote_ep, string msg)
+        {
+            RemoteEP = remote_ep;
+            Msg = msg;
+        }
+
+        public string IPStr
+        {
+            get
+            {
+                return RemoteEP.Address.ToString();
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return RemoteEP.Port;
+            }
+        }
+    }
+}
