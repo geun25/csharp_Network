8e1508fc2c70a4507afbdcd92cdcd440cce9fb49
geun25 <sas0898@naver.com>
2021-09-08 14:53:18 +0900

    닷넷 리모팅

--- 닷넷 리모팅/RemotingClient/Program.cs
+++ 닷넷 리모팅/RemotingClient/Program.cs
@@ -1,11 +1,7 @@
 ﻿using GeneralLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RemotingClient
 {
